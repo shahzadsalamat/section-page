3de3d7d5e5a074b3a305a4e77a75ced08f3c3034
shahzadsalamat <shahzad.salamat@yahoo.com>
2018-10-11 15:49:32 +0300

    addressed the feedback from andrea

--- index.php
+++ index.php
@@ -6,26 +6,66 @@
     <title>Section</title>
     <link rel="stylesheet" type='text/css' href="gb-style.css">
 
-    <?php
-
-session_start();
-
-include_once("section-data/section-data.php");
-
-global $aSectionData;
-
-if (isset($_REQUEST["section"])) {
-// url: http://localhost/globuzzer-homepage-section/?section=322063
-$sSection = $_REQUEST["section"]; // e.g. 322063 for Copenhagen
-$aData = $aSectionData[$sSection];
-?>
+  <?php
+    error_reporting(0);
+    session_start();
+    include_once("section-data/section-data.php");
+    global $aSectionData;
+    if (isset($_REQUEST["section"])) {
+      // url: http://localhost/globuzzer-homepage-section/?section=322063
+    $sSection = $_REQUEST["section"]; // e.g. 322063 for Copenhagen
+    $aData = $aSectionData[$sSection];
+ ?>
 </head>
 <body>
 
-  <div id='gb-app-wrapper'>
-    <div id='header-black-overlay'></div>
- <!-- Nav start-->
- <div class="gb-navbar gb-background-transparent">
+<div class='gb-app-wrapper'>
+  <div class='gb-app-black-overlay'></div>
+    <!-- NavAside start -->
+    <div class='gb-nav-aside gb-gradient-red-black'>
+      <div class='nav-aside-image-background'>
+      </div>
+      <div class='nav-aside-content'>
+        <div class='nav-aside-close'>
+          <svg class='nav-aside-close-icon gb-icon-medium gb-icon-white-opacity-50 gb-phone-hide' version="1.1" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink">
+            <use xlink:href='#close'></use>
+          </svg>
+        </div>
+        <ul class='nav-aside-content-top'>
+
+          <li class='nav-aside-top-list-item'>
+            <a data-scroll-to='about-us' class='nav-aside-link gb-text-white' href='http://www.globuzzer.com/'>
+              <svg class='nav-aside-icon gb-icon-medium gb-icon-white-opacity-50' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <use xlink:href='#home'></use>
+              </svg>  
+              <h5 class='gb-text-uppercase gb-text-white gb-title-tiny gb-text-uppercase'>Home</h5>
+            </a>
+          </li> 
+          <li class='nav-aside-top-list-item'>
+            <a href='https://globuzzer.mn.co/sign_in' class='nav-aside-link gb-text-white' href='#'>
+              <svg class='nav-aside-icon gb-icon-medium gb-icon-white-opacity-50' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <use xlink:href='#calendar'></use>
+              </svg>
+              <h5 class='gb-text-uppercase gb-text-white gb-title-tiny gb-text-uppercase'>sign in</h5>
+            </a>
+          </li>
+        </ul>
+        <div class='nav-aside-content-bottom line-top'>
+          <div class='content-left'>
+            <a href="#">
+              <svg class="gb-logo-large gb-icon-fill-white" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <use xlink:href="#gb-logo-large"></use>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- NavAside End -->
+
+    <!-- Nav start-->
+    <div class="gb-navbar gb-background-transparent">
       <div class="left-content">
         <a href='#' class='gb-desktop-hide nav-burger'>
           <svg class='gb-icon-medium gb-icon-white' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -34,33 +74,47 @@ $aData = $aSectionData[$sSection];
         </a>
       </div>
       <div class='center-content'>
-        <a class='nav-logo' href="#">
+        <a class='nav-logo' href="http://www.globuzzer.com/">
             <svg class="gb-logo-large gb-icon-fill-white" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <use xlink:href="#gb-logo-small"></use>
             </svg>
         </a>
       </div>
       <div class="right-content">
-        <a class='gb-tablet-hide gb-phone-hide gb-paragraph-memdium gb-text-white' href='http://www.globuzzer.com/'>Home</a>
+        <a  class='gb-tablet-hide gb-phone-hide gb-paragraph-memdium gb-text-white' href='http://www.globuzzer.com/'>Home</a>
         <a href='https://globuzzer.mn.co/sign_in' class='gb-text-capitalize gb-text-white gb-paragraph-memdium'>sign in</a>
       </div>
     </div>
     <!-- Nav end-->
 
+   
+    <!-- Page wrapper start -->
+    <div class='gb-page-wrapper'>
+      <!-- Header start-->
+      <div class='header'>
+
+        <div class='header-black-overlay'></div>
+        <!-- CARD TWO STARTS -->
+        <div class="gb-card-two-wrapper"  style="background-image: linear-gradient( to bottom right, rgba(0, 0, 0, 0.0001) 0%, #514848 100% ),url(<?php echo $aData["image"] ?>);">
+          <h2 class="gb-title-xx-large gb-text-uppercase"><?php echo $aData["name"] ?></h2>
+        <div class="gb-card-two-wrapper-description gb-margin-bottom-desktop-32 gb-margin-bottom-tablet-32">
+         <p class="gb-paragraph-medium "><?php echo $aData["description"] ?></p>
+        </div>
+           <input class="gb-btn gb-btn-small gb-btn-white" value="Join" type="button">
+             <div class="gb-avatars">
+                <h4 class="gb-title-medium"><?php echo $aData["numMembers"] ?> Members</h4>
+             </div>
+         </div>
+<!-- CARD TWO ENDS -->
+       
+      </div>
+      <!-- Header end -->
+    
+<!-- ARTICLES STARTS -->
  
-      <div class="gb-card-two-wrapper"  style="background-image: linear-gradient( to bottom right, rgba(0, 0, 0, 0.0001) 0%, #514848 100% ),url(<?php echo $aData["image"] ?>);">
-    <h2 class="gb-title-xx-large gb-text-uppercase"><?php echo $aData["name"] ?></h2>
-    <p class="gb-paragraph-medium gb-margin-bottom-desktop-32 "><?php echo $aData["description"] ?></p>
-    <input class="gb-btn gb-btn-small gb-btn-white" value="Join" type="button">
-    <div class="gb-avatars">
-      <h4 class="gb-title-tiny"><?php echo $aData["numMembers"] ?> Members</h4>
-    </div>
-  </div>
-
-   <div class='gb-card-four-wrapper'>
+<div class='gb-card-four-wrapper'>
          <ul class='gb-card-four-sub-cards'>
-
-<?php
+         <?php
     foreach($aData["articles"] as $article) {
         echo "<li class='gb-card-four-sub-card'>
             <div class='gb-card-four-sub-card-info'>
@@ -89,22 +143,130 @@ $aData = $aSectionData[$sSection];
         ?>
         <?php
         }
-        ?>
-          
+        ?>   
         </ul>
-
-      
-       
       </div>
       
-       </div> 
-     
-      
-   
+ <!-- ARTICLES ENDS -->
+
+      <!-- Footer Start -->
+      <div class="gb-footer gb-background-black-opacity-5">
+        <div class="footer-wrapper">
+          <div class='footer-top-content'>
+            <ul class='footer-nav gb-phone-hide gb-tablet-hide'>
+              <li class="footer-nav-item gb-margin-bottom-8">
+                <a class="gb-text-black-opacity-50 gb-subtitle-medium" href="https://mightynetworks.com/terms_of_use">
+                  Terms &amp; Conditions
+                </a>
+              </li>
+              <li class="footer-nav-item gb-margin-bottom-8">
+                <a class="gb-text-black-opacity-50 gb-subtitle-medium" href="https://mightynetworks.com/privacy_policy">
+                  Privacy Policy
+                </a>
+              </li>
+            </ul>
+            <ul class="footer-contact-list">
+              <li class='footer-contact-item gb-margin-bottom-8'>
+                <a class='footer-contact-link' href='href="https://www.google.com/maps/place/Skyttev%C3%A4gen+29,+133+36+Saltsj%C3%B6baden,+Ruotsi/@59.2707216,18.2862762,17z/data=!3m1!4b1!4m5!3m4!1s0x465f7f0cd4f5b7b3:0x75deb43dec7d4645!8m2!3d59.2707216!4d18.2884649"'>
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-small" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#location"></use>
+                  </svg>
+                  <p class='gb-text-black-opacity-50 gb-subtitle-medium'>Skyttevägen 29, Saltsjöbaden, Sweden</p>
+                </a>
+              </li>
+              <li class='footer-contact-item gb-margin-bottom-8'>
+                <a class='footer-contact-link' href='tel:46735555134'>
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-small" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#phone"></use>
+                  </svg>
+                  <p class='gb-text-black-opacity-50 gb-subtitle-medium'>+46 73 555 5 134</p>
+                </a>
+              </li>
+              <li class='footer-contact-item gb-margin-bottom-8'>
+                <a class='footer-contact-link' href='mailto:info@globuzzer.com'>
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-small" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#envelope"></use>
+                  </svg>
+                  <p class='gb-text-black-opacity-50 gb-subtitle-medium'>info@globuzzer.com</p>
+                </a>
+              </li>
+            </ul>
+            <ul class="footer-social-media-list">
+              <li class="footer-social-media-item">
+                <a class='footer-contact-link' href="https://www.facebook.com/SocialNetworkforStudentsandExpats/?hc_ref=ARTpukVVg9pLASlFEgSEGsyb34HwQIISr3bQWcT-CghqMvRc2ccJmEMvOT4qVL7plus&__xts__[0]=68.ARAied8czZYuUH1SnAAN-tgadfzqVniY7HZvUHyax0rpZF33Xm97dZKIPU_3iBvcxypV5tTXGHDoy1aI-ZKVuoQ2L7nhf-RjbgQrrwX3FR93GfT85bpmIWr8ObZRBGqTdv1-IYLxVty6zPOYRG-0eRTzdoIxCsrQWIt1u_gqNVOKlCpQwLTe&__tn__=%3C-R">
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-medium" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#facebook"></use>
+                  </svg>
+                </a>
+              </li>
+              <li class="footer-social-media-item">
+                <a class='footer-contact-link' href="https://twitter.com/globuzzer">
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-medium" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#twitter"></use>
+                  </svg>
+                </a>
+              </li>
+              <li class="footer-social-media-item">
+                <a class='footer-contact-link' href="https://www.youtube.com/channel/UC4u8N-QBDMWG6OqzSni8clw">
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-medium" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#youtube"></use>
+                  </svg>
+                </a>
+              </li>
+              <li class="footer-social-media-item">
+                <a class='footer-contact-link' href="https://www.linkedin.com/company/globuzzer/">
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-medium" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#linkedin"></use>
+                  </svg>
+                </a>
+              </li>
+              <li class="footer-social-media-item">
+                <a class='footer-contact-link' href="https://www.pinterest.se/globuzzer/pins/">
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-medium" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#pinterest"></use>
+                  </svg>
+                </a>
+              </li>
+              <li class="footer-social-media-item">
+                <a class='footer-contact-link' href="https://www.instagram.com/globuzzer/">
+                  <svg class="gb-icon-fill-black-opacity-30 gb-icon-medium" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <use xlink:href="#instagram"></use>
+                  </svg>
+                </a>
+              </li>
+            </ul>
+
+
+
+          </div>
+          <div class='footer-bottom-content'>
+            <div class="footer-rights-reserved">
+              <div class='left-content'>
+                <p class="gb-label gb-text-black-opacity-30">© 2018 All rights reserved.</p>
+              </div>
+              <ul class='right-content gb-desktop-hide'>
+                <li class="footer-nav-item">
+                  <a class="gb-text-black-opacity-30 gb-subtitle-medium" href="https://mightynetworks.com/terms_of_use">
+                    Terms &amp; Conditions
+                  </a>
+                </li>
+                <li class="footer-nav-item">
+                  <a class="gb-text-black-opacity-30 gb-subtitle-medium" href="https://mightynetworks.com/privacy_policy">
+                    Privacy Policy
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Footer End -->
+    </div>
+  </div>
 
   <script type='text/javascript' src='scripts/svgs-loader.js'></script>
-  <script type='text/javascript' src='scripts/topNavbar.js'></script>
+  <script type='text/javascript' src='scripts/nav-aside-trigger.js'></script>
+  <script type='text/javascript' src='scripts/navbar.js'></script>
   
-
 </body>
 </html>
---
+++ scripts/navbar.js
@@ -0,0 +1,177 @@
+function init(){
+	/* Debouncer */
+	const debounce = (func, wait = 10, immediate = false) => {
+		let timeout;
+		return function () {
+			let context = this, args = arguments;
+			let later = function () {
+				timeout = null;
+				if (!immediate) func.apply(context, args)
+			};
+			let callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		}
+	}
+
+	/* SEARCH BAR */
+	let searchForm = document.querySelector('.gb-search-form');
+
+	searchForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const value = searchForm.querySelector('.search-input').value.replace(/[^a-z0-9]/g, '')
+		if(value){
+			window.open(`https://globuzzer.mn.co/search?term=’${value}’`)
+		}
+	});
+
+	/* Nav Bar */
+	const header = document.querySelector('.header');
+	const navBar = document.querySelector('.gb-navbar');
+	const navBarHeight = navBar.scrollHeight;
+
+	/* Black overlay header */
+	const blackHeaderOverlay = document.querySelector('.header-black-overlay');
+
+	/* Sections holder, auto scroll on X*/
+	const sectionsHolder = document.querySelector('.gb-card-six');
+	let alreadyScrolled = false;
+	let scrollAt = 0;
+	let scrollXinerval;
+
+	let citySectionPassed = sectionsHolder.getBoundingClientRect().y + sectionsHolder.clientHeight/2 < 0; //initial scroll of the page is allready ove it if < 0 is passed
+
+	const scrollSectionX = () => {
+		scrollAt+=2;
+		sectionsHolder.scrollTo({
+			left: scrollAt
+		});
+		let sectionWidth = sectionsHolder.scrollWidth*0.2;
+		sectionWidth = sectionWidth > 150? 150 : sectionWidth;
+		
+		if(scrollAt >= sectionWidth){
+			clearInterval(scrollXinerval)
+		}
+	}
+	//Scroll effect
+	const scrolling = (e) => {
+		/* Script for the blackHeaderOverlay to change it's opacity */
+		const scrollY = window.scrollY;
+		const headerHeight = header.scrollHeight;
+		if(scrollY < headerHeight){
+			blackHeaderOverlay.style.opacity = ((scrollY + 1) /headerHeight )/2
+		}
+		
+
+		/* Script for the nav to be sticky */
+
+		const isPassed = scrollY >= 10;
+		if(isPassed && navBar.classList.contains('gb-background-transparent')){
+			navBar.classList.remove('gb-background-transparent');
+			navBar.classList.add('gb-background-primary' , 'sticky');
+		}else if(!isPassed && navBar.classList.contains('sticky')){
+			navBar.classList.remove('gb-background-primary' , 'sticky')
+			navBar.classList.add('gb-background-transparent');
+		}
+
+
+		/* Script that autoscroll when half of the sections is in the view */
+		if(!alreadyScrolled){
+			let sectionsDistanceFromTop
+			
+			if(citySectionPassed){
+				sectionsDistanceFromTop = - (sectionsHolder.getBoundingClientRect().y + sectionsHolder.clientHeight / 2)
+			}else{
+				sectionsDistanceFromTop = sectionsHolder.getBoundingClientRect().y - window.innerHeight + sectionsHolder.clientHeight/2;
+			}
+
+			
+			if(sectionsDistanceFromTop < 0){
+				alreadyScrolled = true;
+				scrollXinerval = setInterval(scrollSectionX,50)
+			}
+		}
+	}
+	scrolling();
+	window.addEventListener('scroll' , debounce(scrolling));
+
+	//Scroll to element when press on link
+	const navLinks = navBar.querySelectorAll('a');
+	const navAsideLinks = document.querySelectorAll('.nav-aside-content a');
+	const navAside = document.querySelector('.gb-nav-aside');
+	const pageWrapper = document.querySelector('.gb-page-wrapper');
+
+	//Function that scroll to an element when pressing a link from both nav and navaside
+	const scrollToSection = (element) => {
+		const scrollToElement = document.getElementById(element.dataset.scrollTo); //get the element to scrollTo
+		const elementFromTop = scrollToElement.offsetTop; //get the distance from the top of the specific element
+		
+		// The cases when the user is clicking from the navaside links
+		if(pageWrapper.classList.contains('translated')){
+			//close the navAside
+			pageWrapper.classList.remove('translated');
+			navAside.classList.remove('translated');
+
+			//after the transition of the navAside is done scroll to the element
+			pageWrapper.addEventListener('transitionend' , function endingOfTransition(e){
+				if(e.propertyName != 'transform') return;
+				window.scrollTo({
+					top: elementFromTop - navBarHeight + navBarHeight/2,
+					behavior: "smooth"
+				});
+
+				//remove eventlistener
+				pageWrapper.removeEventListener('transitionend', endingOfTransition )
+			})
+		}else{ //if the click comes from the other links just scroll to the element
+			window.scrollTo({
+				top: elementFromTop - navBarHeight + navBarHeight/2,  
+				behavior: "smooth"
+			});
+		}
+	}
+	
+	//add the click listener to all the links in the navBar
+	navLinks.forEach(el => {
+		if(el.dataset.scrollTo){
+			el.addEventListener('click' ,(e) => {
+				e.preventDefault();
+				scrollToSection(el)
+			})
+		}
+	})
+
+	//add the click listener to all the links in the navAside
+	navAsideLinks.forEach(el => {
+		if(el.dataset.scrollTo){
+			el.addEventListener('click' , (e) => {
+				e.preventDefault();
+				scrollToSection(el)
+			}
+		)
+		}
+	})
+
+	//weird no space thing on the right part of the list , fixed here
+	const listToBeFixed = document.querySelector('.card-16-list');
+	const elementToFixWith = listToBeFixed.querySelector('.list-space-right-fix');
+
+	const listToBeFixedListener = () => {
+		const showElementToFixWith = listToBeFixed.clientWidth < listToBeFixed.scrollWidth;
+		const shown = elementToFixWith.classList.contains('shown');
+		if(showElementToFixWith && !shown){
+			elementToFixWith.classList.add('shown')
+		}else if(!showElementToFixWith && shown){
+			elementToFixWith.classList.remove('shown')
+		}
+
+	}
+
+	listToBeFixedListener();
+	window.addEventListener('resize' , debounce(listToBeFixedListener));
+
+
+}
+init();
